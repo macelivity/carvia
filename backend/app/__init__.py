1e9b257e55b3dd933b5d3cc139c471347883c1cd
Lauris <lauris.schleussner@gmail.com>
2025-07-04 19:38:09 +0200

    typo fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -73,7 +73,7 @@ def create_app():
     api.register_blueprint(tarif_bp, url_prefix="/tarif")
     api.register_blueprint(rechnung_bp, url_prefix="/rechnung")
     api.register_blueprint(reservierung_bp, url_prefix="/reservierung")
-    app.register_blueprint(user_bp, url_prefix="/user")
+    api.register_blueprint(user_bp, url_prefix="/user")
 
     app.register_blueprint(api, url_prefix="/api")
 
